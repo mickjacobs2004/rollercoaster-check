54537307f16c6f5f1bfcebd066b2b17527f7a76e
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2021-02-22 13:25:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ while running:
     #Get inputs
     os.system('cls')
     print("Rollercoaster-check™")
-    age = input("Voer leeftijd in: ")
-    height = input("Voer lengte in: ")
-    age = int(age)
-    height = int(height)
+    int age = input("Voer leeftijd in: ")
+    int height = input("Voer lengte in: ")
+
 
     #Process checks
     if(age > age_check and height > height_check):
